e218fb30d35c1dd80c6fc07e3e7c2644b0e6f714
diegobelliardo <57402618+diegobelliardo@users.noreply.github.com>
2020-10-25 18:01:25 +0100

    The new .gitignore is now working

---
+++ BackupMDFDB/BackupMDFDB/Program.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.IO;
+
+
+namespace BackupMDFDB
+{
+    class Program
+    {
+        public const string WORKINGPATH = @"..\..\Data\";
+        private static string DB_PATH = System.Environment.CurrentDirectory;
+        private static string CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + DB_PATH + @"\prova.mdf; Integrated Security=True";
+        private static string RESTORE_CONNECTION_STRING = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=" + DB_PATH + @"\FormulaOne.bak; Integrated Security=True";
+        private static string DB_NAME = @"D:\USERS\DIEGO\DESKTOP\SCUOLA\CSHARP-CODE\BACKUPMDFDB\BACKUPMDFDB\BIN\DEBUG\PROVA.MDF";
+
+        static void Main(string[] args)
+        {
+            char scelta = ' ';
+            do
+            {
+                Console.WriteLine("\n*** BATCH SCRIPTS ***\n");
+                Console.WriteLine("1 - Create Countries");
+                Console.WriteLine("2 - Create Teams");
+                Console.WriteLine("3 - Create Drivers");
+                Console.WriteLine("------------------");
+                Console.WriteLine("B - Backup all");
+                Console.WriteLine("R - Restore");
+                Console.WriteLine("------------------");
+                Console.WriteLine("X - EXIT\n");
+                scelta = Console.ReadKey(true).KeyChar;
+                switch (scelta)
+                {
+                    case '1':
+                        ExecuteSqlScript("Countries.sql");
+                        break;
+                    case '2':
+                        ExecuteSqlScript("Teams.sql");
+                        break;
+                    case '3':
+                        ExecuteSqlScript("Drivers.sql");
+                        break;
+                    case 'B':
+                        Backup();
+                        break;
+                    case 'R':
+                        Restore();
+                        //Restore3();
+                        break;
+                    default:
+                        if (scelta != 'X' && scelta != 'x') Console.WriteLine("\nUncorrect Choice - Try Again\n");
+                        break;
+                }
+            } while (scelta != 'X' && scelta != 'x');
+        }
+
+        private static void Backup()
+        {
+            try
+            {
+                using (SqlConnection dbConn = new SqlConnection())
+                {
+                    dbConn.ConnectionString = CONNECTION_STRING;
+                    dbConn.Open();
+
+                    using (SqlCommand multiuser_rollback_dbcomm = new SqlCommand())
+                    {
+                        multiuser_rollback_dbcomm.Connection = dbConn;
+                        multiuser_rollback_dbcomm.CommandText = @"ALTER DATABASE [" + DB_NAME + "] SET MULTI_USER WITH ROLLBACK IMMEDIATE";
+
+                        multiuser_rollback_dbcomm.ExecuteNonQuery();
+                    }
+                    dbConn.Close();
+                }
+
+                SqlConnection.ClearAllPools();
+
+                using (SqlConnection backupConn = new SqlConnection())
+                {
+                    backupConn.ConnectionString = CONNECTION_STRING;
+                    backupConn.Open();
+
+                    using (SqlCommand backupcomm = new SqlCommand())
+                    {
+                        backupcomm.Connection = backupConn;
+                        backupcomm.CommandText = @"BACKUP DATABASE ["+DB_NAME+"] TO DISK='"+ DB_PATH + @"\prova.bak'";
+                        backupcomm.ExecuteNonQuery();
+                    }
+                    backupConn.Close();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static void Restore()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(CONNECTION_STRING))
+                {
+                    con.Open();
+                    string sqlStmt2 = string.Format(@"ALTER DATABASE [" + DB_NAME + "] SET SINGLE_USER WITH ROLLBACK IMMEDIATE");
+                    SqlCommand bu2 = new SqlCommand(sqlStmt2, con);
+                    bu2.ExecuteNonQuery();
+
+                    string sqlStmt3 = @"USE MASTER RESTORE DATABASE [" + DB_NAME + "] FROM DISK='" + DB_PATH + @"\prova.bak' WITH REPLACE;";
+                    SqlCommand bu3 = new SqlCommand(sqlStmt3, con);
+                    bu3.ExecuteNonQuery();
+
+                    string sqlStmt4 = string.Format(@"ALTER DATABASE [" + DB_NAME + "] SET MULTI_USER");
+                    SqlCommand bu4 = new SqlCommand(sqlStmt4, con);
+                    bu4.ExecuteNonQuery();
+
+                    Console.WriteLine("database restoration done successefully");
+                    con.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+        }
+
+        static void ExecuteSqlScript(string sqlScriptName)
+        {
+            var fileContent = File.ReadAllText(WORKINGPATH + sqlScriptName);
+            fileContent = fileContent.Replace("\r\n", "");
+            fileContent = fileContent.Replace("\r", "");
+            fileContent = fileContent.Replace("\n", "");
+            fileContent = fileContent.Replace("\t", "");
+            var sqlqueries = fileContent.Split(new[] { ";" }, StringSplitOptions.RemoveEmptyEntries);
+
+            var con = new SqlConnection(CONNECTION_STRING);
+            var cmd = new SqlCommand("query", con);
+            con.Open(); int i = 0; int nErr = 0;
+            foreach (var query in sqlqueries)
+            {
+                cmd.CommandText = query; i++;
+                try
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                catch (SqlException err)
+                {
+                    Console.WriteLine("Errore in esecuzione della query numero: " + i);
+                    Console.WriteLine("\tErrore SQL: " + err.Number + " - " + err.Message);
+                    nErr++;
+                }
+            }
+            string finalMessage = nErr == 0 ? "Script completed without errors." : "Script completed with " + nErr + " ERRORS!";
+            Console.WriteLine("\n" + finalMessage + "\n");
+            con.Close();
+        }
+                
+    }
+}
